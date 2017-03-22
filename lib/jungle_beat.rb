e60444f09de0524f7c6db6c87532051fcc74e328
NataliaColome <nmcolome@gmail.com>
2017-03-22 12:48:52 -0600

    update

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,53 +1,26 @@
-require './lib/node'
-require './lib/linked_list.rb'
-require './lib/jungle_beat_class.rb'
+class JungleBeat
 
-# p node = Node.new("plop")
-# node.data
-# node.next_node
+    def initialize(list = LinkedList.new)
+        @list = list
+    end
 
-# p list = LinkedList.new
-# list.head
-# p list.append("doop")
-# list
-# list.head
-# list.head.next_node
-# list.append("deep")
-# list
-# list.head
-# list.head.next_node
-# list.count
-# list.to_string
-# list.append("plop")
-# list.to_string
-# list.append("suu")
-# list
-# list.prepend("dop")
-# list.to_string
-# list.count
-# list.insert(1, "woo")
-# list.to_string
-# list.append("deep")
-# list.insert(1, "shi")
-# list.insert(2, "shu")
-# list.append("blop")
-# list.insert(1,"woo")
-# list.to_string
-# list.find(2, 1)
-# list.find(1, 3)
-# list.includes?("deep")
-# list.includes?("dep")
-# list.count
-# list.pop
-# list.to_string
-# list.pop
-# list.to_string
+    def list
+        @list
+    end
 
-jb = JungleBeat.new
-jb.list
-jb.list.head
-jb.append("deep doo ditt")
-jb.list.head.data
-jb.list.head.next_node.data
-jb.append("woo hoo shu")
-p jb.count
+    def append(string)
+        words = string.split
+        words.each {|word| @list.append(word)}
+    end
+
+    def count
+        @list.count
+        # # list = @list ---- useful for LinkedList method
+        # info = []
+        # while @list.head.next_node != nil
+        #     info << @list.head.data
+        #     @list = @list.head.next_node
+        # end
+        # info.count
+    end    
+end
---
+++ lib/jungle_beat_for_test.rb
@@ -0,0 +1,53 @@
+require './lib/node'
+require './lib/linked_list.rb'
+require './lib/jungle_beat_class.rb'
+
+# p node = Node.new("plop")
+# node.data
+# node.next_node
+
+# p list = LinkedList.new
+# list.head
+# p list.append("doop")
+# list
+# list.head
+# list.head.next_node
+# list.append("deep")
+# list
+# list.head
+# list.head.next_node
+# list.count
+# list.to_string
+# list.append("plop")
+# list.to_string
+# list.append("suu")
+# list
+# list.prepend("dop")
+# list.to_string
+# list.count
+# list.insert(1, "woo")
+# list.to_string
+# list.append("deep")
+# list.insert(1, "shi")
+# list.insert(2, "shu")
+# list.append("blop")
+# list.insert(1,"woo")
+# list.to_string
+# list.find(2, 1)
+# list.find(1, 3)
+# list.includes?("deep")
+# list.includes?("dep")
+# list.count
+# list.pop
+# list.to_string
+# list.pop
+# list.to_string
+
+jb = JungleBeat.new
+jb.list
+jb.list.head
+jb.append("deep doo ditt")
+jb.list.head.data
+jb.list.head.next_node.data
+jb.append("woo hoo shu")
+p jb.count
